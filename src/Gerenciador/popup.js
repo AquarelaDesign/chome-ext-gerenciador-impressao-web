2b27d52eb47f40e1dd6f769345934fa6ea76b141
José Augusto Freire <aquarela.design@gmail.com>
2019-09-11 15:23:43 -0300

    Versão 0.0.1

--- src/Gerenciador/popup.js
+++ src/Gerenciador/popup.js
@@ -6,19 +6,9 @@
    var PDF = '';
    var NOT = '';
    var IP  = '';
-   var PL  = '';
-   var Ip = "";
 
    var response     = document.getElementById('response');
    var searchButton = document.getElementById('buttonSearch');
-
-   getIPs(function(ip){
-      Ip = ip;
-   });
-
-   if (Ip != "" && IP != Ip) {
-      IP = Ip;
-   }
 
    AtualizaGet();
 
@@ -31,17 +21,14 @@
       });
       
       if (URL != "" && URL != null) {
-         if (PL == "") {
-            PL = "siarewebpy.pl";
-         }
-
+        console.log("01=>",IP);
          var url = URL
-                 + '/cgi-bin/' + PL + '/wsgimp'
+                 + 'cgi-bin/siareweb.pl/wsgimp'
                  + '?tipo='
                  + '&ip=' + IP
                  + '&modo='
                  + '&rand=' + Math.floor((Math.random() * 1000000000) + 1);
-         //console.log('01=>URL->', URL, IP);
+         //console.log('01=>URL->', url, ip);
          ajaxGetRequest(url, getList);
       }
    }
@@ -51,8 +38,7 @@
          alerts: NOT,
          pdf: PDF,
          url: URL,
-		   ip: IP,
-		   pl: PL
+		 ip: IP
       }, function(items) {
          //console.log("items", typeof(items.pdf));
 
@@ -72,19 +58,7 @@
          if (items.ip != undefined && items.ip != null) {
             IP = items.ip;
          } else {
-            IP =  Ip;
-         }
-         
-         if (Ip != "" && IP != Ip) {
-            IP = Ip;
-         }
-         
-         //console.log("01=>IP", IP, Ip);
-         
-         if (items.pl != undefined && items.pl != null) {
-            PL = items.pl;
-         } else {
-            PL = "";
+            IP = findIP(addIP);;
          }
 
          if (items.url != undefined && items.url != null) {
@@ -100,28 +74,20 @@
 	  chrome.tabs.getAllInWindow(null, function(tabs) {
          var tabTitle = "";
          var tabId    = 0;
-         var tabUrl   = "";
          for(var i=0;i<tabs.length;i++){
-            if (tabs[i].title == tabTitle && 
-                tabs[i].title != "SiareWeb - Procyon Sistemas" &&
-                tabs[i].url == tabUrl) {
+            if (tabs[i].title == tabTitle && tabs[i].title != "SiareWeb - Procyon Sistemas") {
                chrome.tabs.remove(tabId);
             }
             tabTitle = tabs[i].title;
             tabId = tabs[i].id;
-            tabUrl = tabs[i].url;
-            //console.log(tabs[i]);
+            //console.log(tabs[i].title);
          }
       });
       
-      //console.log("02=>",IP, PL);
+      console.log("02=>",IP);
       if (URL != "" && URL != null) {
-         if (PL == "") {
-            PL = "siareweb.pl"
-         }
-         
          var url = URL
-                 + '/cgi-bin/' + PL + '/wsgimp'
+                 + 'cgi-bin/siareweb.pl/wsgimp'
                  + '?tipo='
                  + '&ip=' + IP
                  + '&modo=';
@@ -172,7 +138,7 @@
 
                            // Marca como ja impresso
                            var url = URL
-                                   + '/cgi-bin/' + PL + '/wsgimp'
+                                   + 'cgi-bin/siareweb.pl/wsgimp'
                                    + '?tipo=M'
                                    + '&ip='   + IP
                                    + '&data=' + DataGer
@@ -210,19 +176,10 @@
 
    // cria a lista com o resultado da busca
    function getList(json) {
-
-      if (Ip != "" && IP != Ip) {
-         IP = Ip;
-      }
-      
-      //console.log("02=>IP", IP);
-
-      document.getElementById("TitTab").innerHTML = "Relat&oacute;rios Gerados ("+IP+")";
-
       if (json != null) {
          if (json.dtbimp !== undefined) {
             AtualizaGet();
-            //console.log('02=> getList');
+            //console.log('01=> getList');
 
             var ttbimp = json.dtbimp.ttbimp;
             
@@ -234,18 +191,14 @@
             
             //var lin = $('<tr></tr>').attr({ class: ["commands-row"].join(' ') }).appendTo(tb);
             var lin = $('<tr></tr>').appendTo("#Tabela");
-            $('<td></td>').text("Arquivo").attr({width:"50%", style: ["font-weight: bold"]}).appendTo(lin);
-            $('<td></td>').text("Data").attr({width:"10%", style: ["font-weight: bold"]}).appendTo(lin);
-            $('<td></td>').text("Hora").attr({width:"10%", style: ["font-weight: bold"]}).appendTo(lin);
-            $('<td></td>').text("IP").attr({width:"20%", style: ["font-weight: bold"]}).appendTo(lin);
-            $('<td></td>').text("Status").attr({width:"10%", style: ["font-weight: bold"]}).appendTo(lin);
+            $('<td></td>').text("Arquivo").appendTo(lin);
+            $('<td></td>').text("Data").appendTo(lin);
+            $('<td></td>').text("Hora").appendTo(lin);
+            $('<td></td>').text("IP").appendTo(lin);
+            $('<td></td>').text("Status").appendTo(lin);
             
-            var x = 0;
-            var lmax = 15;
-
-            //console.log("03=>", lmax);
-
             if (ttbimp != null) {
+               
                for (var i=0; i < ttbimp.length; i++) {
                   var aArqPDF = ttbimp[i].ArqPDF.split("/");
                   var ArqPDF;
@@ -254,7 +207,7 @@
                   }
                   
                   //var lin = $('<tr></tr>').attr({ class: ["commands-row"].join(' ') }).appendTo(tb);
-                  var lin = $('<tr></tr>').appendTo("#Tabela");
+                  var lin = $('<tr></tr>').attr({ class: ["commands-row"].join(' ') }).appendTo("#Tabela");
                   var td = $('<td></td>').text('').appendTo(lin);
                   
                   var link = $('<a />', {href: ttbimp[i].URLArq+ArqPDF}).text(ArqPDF);
@@ -294,7 +247,7 @@
                         if(req.readyState == 4 && req.status == 200) {
                            // Marca como ja impresso
                            var url = URL
-                                   + '/cgi-bin/' + PL + '/wsgimp'
+                                   + 'cgi-bin/siareweb.pl/wsgimp'
                                    + '?tipo=M'
                                    + '&ip='   + IP
                                    + '&data=' + DataGer
@@ -322,29 +275,9 @@
                      req.send(null);
                      
                   }
-                  x = i;
-               } // final do for ttbimp.length
+               } 
                
             } 
-            
-            if (ttbimp != null) {
-               if (ttbimp.length < lmax) {
-                  x = lmax - ttbimp.length;
-               } else {
-                  x = 0;
-               }
-            }
-            
-            //console.log("04=>", lmax, x);
-            for (var y=0;y<=x;y++){
-               var lin = $('<tr></tr>').attr({ class: ["commands-row"].join(' ') }).appendTo("#Tabela");
-               var td = $('<td></td>').text('').appendTo(lin);
-               var td = $('<td></td>').text('').appendTo(lin);
-               var td = $('<td></td>').text('').appendTo(lin);
-               var td = $('<td></td>').text('').appendTo(lin);
-               var td = $('<td></td>').text('').appendTo(lin);
-            }
-               
             $("#loader-container").hide();
          }
       }
@@ -354,9 +287,6 @@
    // funcao para a execucao do ajax
    function ajaxGetRequest(url, callback) {
       var req = new XMLHttpRequest();
-      
-      //console.log("03=>", url);
-      
       req.open("GET",url,true);
       req.onreadystatechange  = function(data)
       {
@@ -422,88 +352,36 @@
      });
    }
   
-   //get the IP addresses associated with an account
-   function getIPs(callback){
+   function findIP(onNewIP) { //  onNewIp - your listener function for new IPs
+      var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection; //compatibility for firefox and chrome
+      var pc = new myPeerConnection({iceServers: []}),
+          noop = function() {},
+          localIPs = {},
+          ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
+          key;
 
-       //console.log("03=>IP", Ip, callback);
+      function ipIterate(ip) {
+         if (!localIPs[ip]) onNewIP(ip);
+            localIPs[ip] = true;
+         }
+         pc.createDataChannel(""); //create a bogus data channel
+         pc.createOffer(function(sdp) {
+            sdp.sdp.split('\n').forEach(function(line) {
+            if (line.indexOf('candidate') < 0) return;
+            line.match(ipRegex).forEach(ipIterate);
+         });
+         
+         pc.setLocalDescription(sdp, noop, noop);
+      }, noop); // create offer and set local description
+      
+      pc.onicecandidate = function(ice) { //listen for candidate events
+         if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
+         ice.candidate.candidate.match(ipRegex).forEach(ipIterate);
+      };
+   }  
 
-       var ip_dups = {};
-
-       //compatibility for firefox and chrome
-       var RTCPeerConnection = window.RTCPeerConnection
-           || window.mozRTCPeerConnection
-           || window.webkitRTCPeerConnection;
-       var useWebKit = !!window.webkitRTCPeerConnection;
-
-       //bypass naive webrtc blocking using an iframe
-       if(!RTCPeerConnection){
-           //NOTE: you need to have an iframe in the page right above the script tag
-           //
-           //<iframe id="iframe" sandbox="allow-same-origin" style="display: none"></iframe>
-           //<script>...getIPs called in here...
-           //
-           var win = iframe.contentWindow;
-           RTCPeerConnection = win.RTCPeerConnection
-               || win.mozRTCPeerConnection
-               || win.webkitRTCPeerConnection;
-           useWebKit = !!win.webkitRTCPeerConnection;
-       }
-
-       //minimal requirements for data connection
-       var mediaConstraints = {
-           optional: [{RtpDataChannels: true}]
-       };
-
-       var servers = {iceServers: [{urls: "stun:stun.services.mozilla.com"}]};
-
-       //construct a new RTCPeerConnection
-       var pc = new RTCPeerConnection(servers, mediaConstraints);
-
-       function handleCandidate(candidate){
-           //match just the IP address
-           var ip_regex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/
-           //var ip_addr = ip_regex.exec(candidate)[1];
-           var ip_addr;
-           if (ip_regex.exec(candidate) != null || ip_regex.exec(candidate) != undefined) {
-              ip_addr = ip_regex.exec(candidate)[1];
-
-              //remove duplicates
-              if(ip_dups[ip_addr] === undefined)
-                  callback(ip_addr);
-
-              ip_dups[ip_addr] = true;
-            }
-       }
-
-       //listen for candidate events
-       pc.onicecandidate = function(ice){
-
-           //skip non-candidate events
-           if(ice.candidate)
-               handleCandidate(ice.candidate.candidate);
-       };
-
-       //create a bogus data channel
-       pc.createDataChannel("");
-
-       //create an offer sdp
-       pc.createOffer(function(result){
-
-           //trigger the stun server request
-           pc.setLocalDescription(result, function(){}, function(){});
-
-       }, function(){});
-
-       //wait for a while to let everything done
-       setTimeout(function(){
-           //read candidate info from local description
-           var lines = pc.localDescription.sdp.split('\n');
-
-           lines.forEach(function(line){
-               if(line.indexOf('a=candidate:') === 0)
-                   handleCandidate(line);
-           });
-       }, 1000);
+   function addIP(Ip) {
+      ip = Ip;
    }
 
    checkAlarm();
